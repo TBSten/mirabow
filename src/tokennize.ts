0097371266b3639214da4ab1f76061ca8f26c7b8
TBSten <programmingcafeteria@gmail.com>
2022-07-20 22:03:20 +0900

    [fix] getIsKeywordsの出力順を逆転

--- src/tokennize.ts
+++ src/tokennize.ts
@@ -2,7 +2,7 @@ import { esc } from "./helper/escape"
 import { Tokens } from "./types"
 
 const _isKeywords = new Set<string>()
-export const getIsKeywords = () => Array.from(_isKeywords.values()).sort((a, b) => a.length - b.length)
+export const getIsKeywords = () => Array.from(_isKeywords.values()).sort((a, b) => b.length - a.length)
 export const addIsKeywords = (isKeywords: string) => _isKeywords.add(isKeywords)
 
 export const tokennize = (source: string, keywords: string[] = Array.from(_isKeywords)): Tokens => {
