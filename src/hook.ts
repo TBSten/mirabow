43ea8f2cda7fa54d8fa4c343223224ed9b2c8387
TBSten <programmingcafeteria@gmail.com>
2022-07-22 23:18:37 +0900

    [add] addHooks関数を追加

--- src/hook.ts
+++ src/hook.ts
@@ -4,4 +4,9 @@ const _hooks: Record<string, Hook> = {}
 export const addHook = (name: string, hook: Hook) => {
     _hooks[name] = hook
 }
+export const addHooks = (hooks: Record<string, Hook>) => {
+    Object.entries(hooks).forEach(([name, hook]) => {
+        addHook(name, hook)
+    })
+}
 export const _getAllHooks = () => _hooks
--- test/hook.test.ts
+++ test/hook.test.ts
@@ -15,3 +15,4 @@ test("hook", () => {
     expect(tmp)
         .toBe("b")
 })
+
