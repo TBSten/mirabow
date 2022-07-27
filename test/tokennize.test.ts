7d11bbbd8c68edb0fd83345b807e87dae41f5dc7
TBSten <programmingcafeteria@gmail.com>
2022-07-28 03:22:36 +0900

    [fix] tokennizeのテストにテストケースと追加

--- test/tokennize.test.ts
+++ test/tokennize.test.ts
@@ -1,4 +1,4 @@
-import { is, or, tokennize } from "../src"
+import { def, is, li, or, ref, tokennize, toMatcher } from "../src"
 
 test("tokennize with string keywords", () => {
     or("a", "b", "c")
@@ -13,5 +13,31 @@ test("tokennize with regex keywords", () => {
     or("a", "bb", is(/"xxx"/))
     expect(tokennize(`abb"xxx"dbb`))
         .toEqual(["a", "bb", `"xxx"`, "d", "bb"])
+})
+test("json tokennize", () => {
+    const str = is(/".*?"/)
+    const num = is(/[0-9]+(.[0-9]+])?/)
+    const bool = or("true", "false")
+    const obj = toMatcher(
+        "{",
+        li([str, ":", ref("value")], ","),
+        "}",
+    )
+    const arr = toMatcher(
+        "[",
+        li(ref("value"), ","),
+        "]",
+    )
+    def("value")(or(
+        str, num, bool, obj, arr,
+    ))
+    expect(tokennize(`{"xxx":""}`))
+        .toEqual([
+            "{",
+            `"xxx"`,
+            ":",
+            `""`,
+            "}",
+        ])
 })
 
