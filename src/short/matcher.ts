2120159f58590b5f3699c99e4c9b9c3a23d93d72
TBSten <programmingcafeteria@gmail.com>
2022-09-17 12:58:44 +0900

    [fix] 数値matcherで0がマッチしないバグを修正

--- src/short/matcher.ts
+++ src/short/matcher.ts
@@ -9,5 +9,5 @@ export const re = repeat
 
 export const enclosedToken = (encloser: string) => is(new RegExp(`${encloser}.*?${encloser}`))
 export const stringLiteral = () => or(enclosedToken(`"`), enclosedToken(`'`),)
-export const integerLiteral = is(/[1-9][0-9]*/)
-export const numberLiteral = is(/[1-9][0-9]*(\.[1-9][0-9]*)?/)
+export const integerLiteral = is(/(0|[1-9][0-9])*/)
+export const numberLiteral = is(/(0|[1-9][0-9])*(\.(0|[1-9][0-9])*)?/)
