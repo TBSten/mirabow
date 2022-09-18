5e4ca9861c3390aa079d15c2e3e8aec12bd39bad
TBSten <programmingcafeteria@gmail.com>
2022-09-18 23:58:36 +0900

    [fix] blankの実装をis("")へ変更

--- src/matcher/blank.ts
+++ src/matcher/blank.ts
@@ -1,24 +1,25 @@
 import { Matcher } from "../type";
-import { tokens } from "../util";
+import { is } from "./is";
 
-export const blank = (): Matcher<"blank"> => {
-    return {
-        debug: `(blank)`,
-        type: `blank`,
-        lex(input) {
-            return {
-                ok: true,
-                tokens: tokens(input.raw, []),
-                end: input.start,
-            }
-        },
-        exec(input) {
-            return {
-                ok: true,
-                capture: {},
-                match: tokens(input.getRaw(), []),
-                raw: input.getRaw(),
-            }
-        },
-    }
+export const blank = (): Matcher<"is"> => {
+    // return {
+    //     debug: `(blank)`,
+    //     type: `blank`,
+    //     lex(input) {
+    //         return {
+    //             ok: true,
+    //             tokens: tokens(input.raw, []),
+    //             end: input.start,
+    //         }
+    //     },
+    //     exec(input) {
+    //         return {
+    //             ok: true,
+    //             capture: {},
+    //             match: tokens(input.getRaw(), []),
+    //             raw: input.getRaw(),
+    //         }
+    //     },
+    // }
+    return is("")
 }
