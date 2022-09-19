c9b5e90476679781bb29a3b3cf5f84775c47f0e1
TBSten <programmingcafeteria@gmail.com>
2022-09-20 02:28:25 +0900

    [fix] stringLiteralの定義を変更

--- src/short/matcher.ts
+++ src/short/matcher.ts
@@ -8,6 +8,6 @@ export const opt = optional
 export const re = repeat
 
 export const enclosedToken = (encloser: string) => is(new RegExp(`${encloser}.*?${encloser}`))
-export const stringLiteral = () => or(enclosedToken(`"`), enclosedToken(`'`),)
+export const stringLiteral = or(enclosedToken(`"`), enclosedToken(`'`),)
 export const integerLiteral = is(/(0|[1-9][0-9]*)/)
 export const numberLiteral = is(/(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))?/)
